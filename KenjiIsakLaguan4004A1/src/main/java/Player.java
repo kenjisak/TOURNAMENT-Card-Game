04331b9c7909f92710638623ed8b32b85a145632
Kenji Isak Laguan <120000674+kenjisak@users.noreply.github.com>
2023-10-09 20:58:28 -0400

    U-TEST-004: Test if Player initializes correctly.

--- KenjiIsakLaguan4004A1/src/main/java/Player.java
+++ KenjiIsakLaguan4004A1/src/main/java/Player.java
@@ -1,2 +1,20 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Player {
+    private String name;
+    private int healthPoints;
+    private int totalInjuryPoints;
+    private List<Card> deckInHand;
+    private List<Card> injuryDeck;
+
+    public Player(String inputName,int inputHealthPoints){
+
+    }
+    public String getName(){ return null; }
+    public int getHealthPoints(){ return -1;}
+    public int getTotalInjuryPoints(){ return -1; }
+    public List<Card> getDeckInHand(){ return null; }
+    public List<Card> getInjuryDeck(){ return null; }
 }
+
--- KenjiIsakLaguan4004A1/src/test/java/PlayerUnitTest.java
+++ KenjiIsakLaguan4004A1/src/test/java/PlayerUnitTest.java
@@ -1,5 +1,18 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
-class PlayerUnitTest {
+public class PlayerUnitTest {
+    @Test
+    @DisplayName("U-TEST-004: Test if Player initializes correctly.")
+    void testPlayerCreation(){
+        Player testPlayer = new Player("Test",50);
 
+        assertEquals("Test", testPlayer.getName());
+        assertEquals(50,testPlayer.getHealthPoints());
+        assertEquals(0,testPlayer.getTotalInjuryPoints());
+        assertEquals(0,testPlayer.getDeckInHand().size());
+        assertEquals(0,testPlayer.getInjuryDeck().size());
+    }
 }
