66fd81b2b245b139b3b79b3672187855a1da4993
Kenji Isak Laguan <120000674+kenjisak@users.noreply.github.com>
2023-10-09 20:17:56 -0400

    CODE-001:  Test if a Basic Weapon Card initializes correctly, poison and none poisoned.

--- KenjiIsakLaguan4004A1/src/main/java/Card.java
+++ KenjiIsakLaguan4004A1/src/main/java/Card.java
@@ -6,12 +6,23 @@ public class Card {
     private int injuryPoints;
 
     public Card(String cardType, String suit, int value) {
+        //cardType == Basic
+        this.type = cardType;
+        this.suit = suit;
+        this.value = value;
 
+        if ((suit.equals("Swords") && (value >= 6 && value <= 9)) || (suit.equals("Arrows") && (value >= 8 && value <= 11)) || (suit.equals("Sorcery") && (value == 5 || value == 6 || value == 10 || value == 11)) || (suit.equals("Deception") && (value == 6 || value == 7 || value == 9 || value == 10)) ){
+            this.isPoisoned = true;//checks if value and suit of card is appropriate to be poisoned
+            this.injuryPoints = 10;
+        } else {//none poisoned card
+            this.isPoisoned = false;
+            this.injuryPoints = 5;
+        }
     }
     //getters
-    public String getType(){ return null; }
-    public String getSuit(){ return null; }
-    public int getValue(){ return -1; }
-    public Boolean getIsPoisoned(){ return null; }
-    public int getInjuryPoints(){ return -1; }
+    public String getType(){ return this.type; }
+    public String getSuit(){ return this.suit; }
+    public int getValue(){ return this.value; }
+    public Boolean getIsPoisoned(){ return this.isPoisoned; }
+    public int getInjuryPoints(){ return this.injuryPoints; }
 }
