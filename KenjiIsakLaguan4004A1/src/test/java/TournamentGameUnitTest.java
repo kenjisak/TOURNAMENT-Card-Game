533747fe342029ad3b86742138feb0af0c7fe2c3
Kenji Isak Laguan <120000674+kenjisak@users.noreply.github.com>
2023-10-09 21:22:49 -0400

    U-TEST-006: Test if a Tournament's Game Deck initializes correctly.

--- KenjiIsakLaguan4004A1/src/test/java/TournamentGameUnitTest.java
+++ KenjiIsakLaguan4004A1/src/test/java/TournamentGameUnitTest.java
@@ -1,6 +1,8 @@
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.Objects;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class TournamentGameUnitTest {
@@ -17,5 +19,31 @@ class TournamentGameUnitTest {
         for (int i = 0; i < 3; i++) {//test if names were correct, dont need to check the rest prev UTESTs covered it
             assertEquals(String.valueOf(i + 1),testGame.players[i].getName());
         }
+    }
+    @Test
+    @DisplayName("U-TEST-006: Test if a Tournament's Game Deck initializes correctly.")
+    void testGameCreationGameDeck(){
+        TournamentGame testGame = new TournamentGame(3,new String[]{"1", "2", "3"},50);
+
+        int swordsCount = 0, arrowsCount = 0, sorceryCount = 0, deceptionCount = 0;
+        int merlinCount = 0, apprenticeCount = 0, alchemyCount = 0;
+
+        for (Card testCard: testGame.gameDeck){
+            if(Objects.equals(testCard.getSuit(), "Swords")){ swordsCount++; }
+            if(Objects.equals(testCard.getSuit(), "Arrows")){ arrowsCount++; }
+            if(Objects.equals(testCard.getSuit(), "Sorcery")){ sorceryCount++; }
+            if(Objects.equals(testCard.getSuit(), "Deception")){ deceptionCount++; }
+
+            if(Objects.equals(testCard.getType(), "Merlin")){ merlinCount++; }
+            if(Objects.equals(testCard.getType(), "Apprentice")){ apprenticeCount++; }
+            if(Objects.equals(testCard.getType(), "Alchemy")){ alchemyCount++; }
+        }
+        assertEquals(15,swordsCount);
+        assertEquals(15,arrowsCount);
+        assertEquals(15,sorceryCount);
+        assertEquals(15,deceptionCount);
+        assertEquals(3,merlinCount);
+        assertEquals(2,apprenticeCount);
+        assertEquals(15,alchemyCount);
     }
 }
