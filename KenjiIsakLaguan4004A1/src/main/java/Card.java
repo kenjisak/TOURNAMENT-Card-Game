f4284d339cbfc14779f277b09b74bd1f412bfc98
Kenji Isak Laguan <120000674+kenjisak@users.noreply.github.com>
2023-10-09 20:09:36 -0400

    U-TEST-001: Test if a Basic Weapon Card initializes correctly, poison and none poisoned.

--- KenjiIsakLaguan4004A1/src/main/java/Card.java
+++ KenjiIsakLaguan4004A1/src/main/java/Card.java
@@ -1,2 +1,17 @@
 public class Card {
+    private String type;//Basic, Me,Ap,Al
+    private String suit;//Sw,Ar,So,De, or empty if not Basic
+    private int value;
+    private boolean isPoisoned;
+    private int injuryPoints;
+
+    public Card(String cardType, String suit, int value) {
+
+    }
+    //getters
+    public String getType(){ return null; }
+    public String getSuit(){ return null; }
+    public int getValue(){ return -1; }
+    public Boolean getIsPoisoned(){ return null; }
+    public int getInjuryPoints(){ return -1; }
 }
--- KenjiIsakLaguan4004A1/src/test/java/CardUnitTest.java
+++ KenjiIsakLaguan4004A1/src/test/java/CardUnitTest.java
@@ -1,5 +1,25 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
-class CardUnitTest {
+public class CardUnitTest {
 
+    @Test
+    @DisplayName("U-TEST-001: Test if a Basic Weapon Card initializes correctly, poison and none poisoned.")
+    void testCardCreation(){
+        Card testBasicNonPoisoned = new Card("Basic", "Swords", 1);
+        assertEquals("Basic", testBasicNonPoisoned.getType());
+        assertEquals("Swords", testBasicNonPoisoned.getSuit());
+        assertEquals(1,testBasicNonPoisoned.getValue());
+        assertEquals(false,testBasicNonPoisoned.getIsPoisoned());
+        assertEquals(5,testBasicNonPoisoned.getInjuryPoints());
+
+        Card testBasicPoisoned = new Card("Basic", "Arrows", 8);
+        assertEquals("Basic", testBasicPoisoned.getType());
+        assertEquals("Arrows", testBasicPoisoned.getSuit());
+        assertEquals(8,testBasicPoisoned.getValue());
+        assertEquals(true,testBasicPoisoned.getIsPoisoned());
+        assertEquals(10,testBasicPoisoned.getInjuryPoints());
+    }
 }
