30d6902a2c5b9aafd9bcfc8e5f03fb95c13990f4
Kenji Isak Laguan <120000674+kenjisak@users.noreply.github.com>
2023-10-09 21:16:21 -0400

    U-TEST-005: Test if a Tournament Game initializes correctly, except the Game Deck.

--- KenjiIsakLaguan4004A1/src/main/java/TournamentGame.java
+++ KenjiIsakLaguan4004A1/src/main/java/TournamentGame.java
@@ -1,2 +1,16 @@
+import java.util.*;
+
 public class TournamentGame {
+    public Player[] players;//keeps track of turns as well by creating players in order of input
+    public String currLeader;
+    public List<Card> gameDeck;
+    public Map<Player, Card> currMeleeCardsPlayed;
+    public int roundNum;
+
+    public TournamentGame(int numPlayers, String[] namesOfPlayers, int initHealthPoints){
+
+    }
+    public static void main(String[] args) {
+
+    }
 }
--- KenjiIsakLaguan4004A1/src/test/java/TournamentGameUnitTest.java
+++ KenjiIsakLaguan4004A1/src/test/java/TournamentGameUnitTest.java
@@ -1,5 +1,21 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class TournamentGameUnitTest {
+    @Test
+    @DisplayName("U-TEST-005: Test if a Tournament Game initializes correctly, except the Game Deck.")
+    void testGameCreation(){
+        TournamentGame testGame = new TournamentGame(3,new String[]{"1", "2", "3"},50);
 
+        assertEquals(3,testGame.players.length);
+        assertEquals("1",testGame.currLeader);
+        assertEquals(0,testGame.currMeleeCardsPlayed.size());
+        assertEquals(1,testGame.roundNum);
+
+        for (int i = 0; i < 3; i++) {//test if names were correct, dont need to check the rest prev UTESTs covered it
+            assertEquals(String.valueOf(i + 1),testGame.players[i].getName());
+        }
+    }
 }
