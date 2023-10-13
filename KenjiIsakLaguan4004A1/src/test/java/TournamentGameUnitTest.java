d23e543ddd9bd56c5cc811241bdec421d436836a
Kenji Isak Laguan <120000674+kenjisak@users.noreply.github.com>
2023-10-12 22:41:25 -0400

    U-TEST-035: Test if the loser's receives the melee deck onto their injury deck correctly.

--- KenjiIsakLaguan4004A1/src/test/java/TournamentGameUnitTest.java
+++ KenjiIsakLaguan4004A1/src/test/java/TournamentGameUnitTest.java
@@ -473,4 +473,27 @@ class TournamentGameUnitTest {
 
         assertEquals("3",testGame.currLeader);
     }
+    @Test
+    @DisplayName("U-TEST-035: Test if the loser's receives the melee deck onto their injury deck correctly.")
+    void testMeleeLoserUpdateDMGDeck() {
+        String[] testPlayersNames = {"1", "2", "3"};
+        TournamentGame testGame = new TournamentGame(3, testPlayersNames, 50);
+
+        testGame.players[0].addToHand(new Card("Alchemy", 1));
+        testGame.players[1].addToHand(new Card("Basic", "Arrows", 1));
+        testGame.players[2].addToHand(new Card("Basic", "Arrows", 2));
+
+        int playerInput = 0;
+        Card chosenCard0 = testGame.players[0].getDeckInHand().get(playerInput);
+        Card chosenCard1 = testGame.players[1].getDeckInHand().get(playerInput);
+        Card chosenCard2 = testGame.players[2].getDeckInHand().get(playerInput);
+
+        testGame.addChosenCard(0,0,chosenCard0);
+        testGame.addChosenCard(1,0,chosenCard1);
+        testGame.addChosenCard(2,0,chosenCard2);
+        testGame.loser = testGame.players[2];//rig "3" to be the loser
+        testGame.meleeLoserUpdate();
+
+        assertEquals(new ArrayList<>(testGame.currMeleeCardsPlayed.values()),testGame.players[2].getInjuryDeck());
+    }
 }
