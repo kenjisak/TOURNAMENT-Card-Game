88a20d0639d7e9e32f4bf770d26aebc04f7e2ce6
Kenji Isak Laguan <120000674+kenjisak@users.noreply.github.com>
2023-10-14 00:51:25 -0400

    A-TEST-002: Scenario 2 Round shuffles and deals to the Players and displays their initial hand and health.

--- KenjiIsakLaguan4004A1/src/test/java/GameAcceptanceTest.java
+++ KenjiIsakLaguan4004A1/src/test/java/GameAcceptanceTest.java
@@ -1,5 +1,6 @@
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestClassOrder;
 
 import java.io.PrintWriter;
 import java.util.Scanner;
@@ -23,5 +24,33 @@ public class GameAcceptanceTest {
         assertEquals("A",TournamentGame.tournamentGame.players[0].getName());
         assertEquals("B",TournamentGame.tournamentGame.players[1].getName());
         assertEquals("C",TournamentGame.tournamentGame.players[2].getName());
+    }
+    @Test
+    @DisplayName("A-TEST-002: Scenario 2 Round shuffles and deals to the Players and displays their initial hand and health.")
+    void ATEST_002(){
+        String[] testPlayersNames = {"1","2","3"};
+        TournamentGame testGame = new TournamentGame(3, testPlayersNames,50);
+
+        testGame.distributePlayersHands();
+
+        assertEquals(80,testGame.gameDeck.size());
+        for (Player currPlayer: testGame.players) {//check each player handed 12 cards
+            assertEquals(12, currPlayer.getDeckInHand().size());
+        }
+
+        for (Player currPlayer: testGame.players) {//clears their hands for ease of testing printing their initial hand
+            currPlayer.getDeckInHand().clear();
+        }
+        testGame.players[0].addToHand(new Card("Basic","Deception",12));
+        testGame.players[1].addToHand(new Card("Basic","Sorcery",6));
+        testGame.players[2].addToHand(new Card("Alchemy",3));
+
+        String expectedOutput = """
+
+                Player 1's Hand: [De(12)] Health Points: 50
+                Player 2's Hand: [So(6)] Health Points: 50
+                Player 3's Hand: [Al(3)] Health Points: 50""";
+
+        assertEquals(expectedOutput, testGame.displayAllPlayersHandsHP());
     }
 }
