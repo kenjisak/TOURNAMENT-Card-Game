938d7d58734410e07189ed8adda8bcc88be1400c
Kenji Isak Laguan <120000674+kenjisak@users.noreply.github.com>
2023-10-15 17:45:25 -0400

    REFAC-039: Fixed Wrong Poison number for Sorcery.

--- KenjiIsakLaguan4004A1/src/main/java/Card.java
+++ KenjiIsakLaguan4004A1/src/main/java/Card.java
@@ -13,7 +13,7 @@ public class Card {
         this.suit = suit;
         this.value = value;
 
-        if ((suit.equals("Swords") && (value >= 6 && value <= 9)) || (suit.equals("Arrows") && (value >= 8 && value <= 11)) || (suit.equals("Sorcery") && (value == 5 || value == 6 || value == 10 || value == 11)) || (suit.equals("Deception") && (value == 6 || value == 7 || value == 9 || value == 10)) ){
+        if ((suit.equals("Swords") && (value >= 6 && value <= 9)) || (suit.equals("Arrows") && (value >= 8 && value <= 11)) || (suit.equals("Sorcery") && (value == 5 || value == 6 || value == 11 || value == 12)) || (suit.equals("Deception") && (value == 6 || value == 7 || value == 9 || value == 10)) ){
             this.isPoisoned = true;//checks if value and suit of card is appropriate to be poisoned
             this.injuryPoints = 10;
         } else {//none poisoned card
