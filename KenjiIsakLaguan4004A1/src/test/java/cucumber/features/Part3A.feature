c07adf271bc32412fa50b7730066572daad182ad
Kenji Isak Laguan <120000674+kenjisak@users.noreply.github.com>
2023-11-04 23:37:03 -0400

    A2-PART3-002: Scenario A completed

--- KenjiIsakLaguan4004A1/src/test/java/cucumber/features/Part3A.feature
+++ KenjiIsakLaguan4004A1/src/test/java/cucumber/features/Part3A.feature
@@ -1,7 +1,57 @@
-Feature: scenario A
+Feature: No Winners Scenario A
   Game ends with no winner
   
-  Scenario: There are 2 rounds with 1 melee each played out and the game ends with no winners
+  Scenario: There are 2 rounds played and the game ends with no winners
     Given Game initializes with 50 hp for 3 players
-    And Round 1 starts as well as distributes each players cards
-    And the Melee 1 starts
+    And Round 1 starts, as well as distributes each players cards
+    And player 1 is rigged with "Sorcery_1,Swords_1" cards
+    And player 2 is rigged with "Alchemy_1,Alchemy_2" cards
+    And player 3 is rigged with "Merlin,Merlin" cards
+
+    When the Melee 1 starts
+    Then "P1" is the current leader
+    When player 1 plays their "Sorcery_1" card
+    And player 2 plays their "Alchemy_1" card
+    And player 3 plays their "Merlin_1" card
+    Then there is No loser for this melee
+
+    When the Melee 2 starts
+    Then "P1" is the current leader
+    When player 1 plays their "Swords_1" card
+    And player 2 plays their "Alchemy_2" card
+    And player 3 plays their "Merlin_3" card
+    Then "P1" will be the loser for this melee
+
+    When Round 1 is over
+    Given Round 2 starts, as well as distributes each players cards
+    And player 1 is rigged with "Swords_6,Arrows_9,Merlin,Sorcery_11" cards
+    And player 2 is rigged with "Swords_7,Arrows_10,Merlin,Sorcery_12" cards
+    And player 3 is rigged with "Swords_8,Arrows_8,Deception_5,Sorcery_6" cards
+
+    When the Melee 1 starts
+    Then "P2" is the current leader
+    When player 2 plays their "Swords_7" card
+    And player 3 plays their "Swords_8" card
+    And player 1 plays their "Swords_6" card
+    Then "P1" will be the loser for this melee
+
+    When the Melee 2 starts
+    And player 1 plays their "Arrows_9" card
+    And player 2 plays their "Arrows_10" card
+    And player 3 plays their "Arrows_8" card
+    Then "P3" will be the loser for this melee
+
+    When the Melee 2 starts
+    And player 3 plays their "Deception_5" card
+    And player 1 plays their "Merlin_10" card
+    And player 2 plays their "Merlin_3" card
+    Then "P2" will be the loser for this melee
+
+    When the Melee 3 starts
+    And player 2 plays their "Sorcery_12" card
+    And player 3 plays their "Sorcery_6" card
+    And player 1 plays their "Sorcery_11" card
+    Then "P3" will be the loser for this melee
+
+    When Round 2 is over
+    Then the Game is over with no winners
--- KenjiIsakLaguan4004A1/src/test/java/cucumber/stepdefinitions/Part3A.java
+++ KenjiIsakLaguan4004A1/src/test/java/cucumber/stepdefinitions/Part3A.java
@@ -2,9 +2,16 @@ package cucumber.stepdefinitions;
 
 import game.*;
 import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 import java.io.PrintWriter;
 import java.io.StringWriter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 import java.util.Scanner;
 
 public class Part3A {
@@ -31,15 +38,97 @@ public class Part3A {
         testGame = TournamentGame.tournamentGame;
         System.out.println();
     }
-    @Given("Round {int} starts as well as distributes each players cards")
+    @Given("Round {int} starts, as well as distributes each players cards")
     public void roundStarts(int roundNum) {
         //simulates beginning of round, since we cant call play round directly.
         System.out.println("\nRound " + testGame.roundNum + " Starting... Initial Leader of this round is " + testGame.currLeader);
         testGame.distributePlayersHands();
         System.out.println(testGame.displayAllPlayersHandsHP());//display each player's initial hands
     }
-    @Given("the Melee {int} starts")
+    @When("the Melee {int} starts")
     public void meleeStarts(int meleeNum) {
         System.out.println("\nRound " + testGame.roundNum + ", Melee " + meleeNum + " Starting...");
+    }
+
+    @Then("{string} is the current leader")
+    public void isTheCurrentLeader(String leaderName) {
+        assertEquals(leaderName,testGame.currLeader);
+    }
+
+    @Given("player {int} is rigged with {string} cards")
+    public void playerIsRiggedWithCards(int playerNum, String hand) {
+        List<Card> riggedHand = new ArrayList<>();
+        String[] strPlyrcard = hand.split(",");
+
+        for(String currCard: strPlyrcard){
+            riggedHand.add(createCard(currCard));
+        }
+
+        int playersIndex = playerNum - 1;
+
+        testGame.players[playersIndex].rigHand(riggedHand);
+        System.out.println("RIGGED: " + testGame.players[playersIndex].displayHand());
+    }
+
+    @When("player {int} plays their {string} card")
+    public void playerPlaysTheirCard(int plyrNum, String plyrCard) {
+        if (plyrCard.contains("Merlin") || plyrCard.contains("Apprentice")) {
+            String[] strPlyrcard = plyrCard.split("_");
+            if (Objects.equals(testGame.currLeader, testGame.players[plyrNum - 1].getName())) {//if player is the Leader and playing MeAp
+                plyrsInput += "0\n" + strPlyrcard[1] + "\n" + strPlyrcard[2] + "\n";//Suit of MeAp card, then value
+            } else {
+                plyrsInput += "0\n" + strPlyrcard[1] + "\n";//value of MeAp card
+            }
+        } else {//basic and alchemy cards, no input needed just index
+            plyrsInput += "0\n";
+        }
+    }
+
+    @Then("there is No loser for this melee")
+    public void thereIsNoLoserForThisMelee() {
+        testGame.playMelee(new Scanner(plyrsInput),new PrintWriter(output));
+        plyrsInput = "";//reset input for next melee
+
+        assertNull(testGame.loser);
+    }
+
+    @Then("{string} will be the loser for this melee")
+    public void willBeTheLoserForThisMelee(String plyrName) {
+        testGame.playMelee(new Scanner(plyrsInput),new PrintWriter(output));
+        plyrsInput = "";//reset input for next melee
+
+        assertEquals(plyrName, testGame.loser.getName());
+    }
+
+    @When("Round {int} is over")
+    public void roundIsOver(int roundNum) {
+        System.out.println("\nRound " + roundNum + " over...");
+        testGame.playersTakeDmg();
+        System.out.println(testGame.displayAllPlayersHP());
+
+        testGame.updateRoundLeader();//else update leader and continue round
+    }
+
+    @Then("the Game is over with no winners")
+    public void theGameIsOverWithNoWinners() {
+        String endOutput = testGame.endGame();
+        System.out.println(endOutput);
+
+        assertTrue(endOutput.contains("no winners"));
+    }
+
+    /////////////HELPER FUNCTIONS/////////////
+    public Card createCard(String givenCard){
+        if (givenCard.contains("Alchemy")){
+            int cardValue = Integer.parseInt(givenCard.split("_")[1]);
+            return new Card("Alchemy",cardValue);
+        } else if (givenCard.contains("Merlin") || givenCard.contains("Apprentice")) {
+            String meApType = givenCard.split("_")[0];
+            return new Card(meApType);
+        }else{//is a basic weapon card
+            String cardSuit = givenCard.split("_")[0];
+            int cardValue = Integer.parseInt(givenCard.split("_")[1]);
+            return new Card("Basic",cardSuit,cardValue);
+        }
     }
 }
